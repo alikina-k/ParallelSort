bf4b7fa84437e35bd87eeb12deb7805a6b330943
alikina-k <81692525+alikina-k@users.noreply.github.com>
2021-04-10 18:56:08 +0300

    Параллельная реализация
    Параллельная сортировка методом чет-нечетной перестановки

--- sort.py
+++ sort.py
@@ -1,6 +1,10 @@
-import random
+# Параллельная сортировка методом чет-нечетной перестановки
 
-# описание массива в виде класса
+import random
+import threading
+import multiprocessing.dummy
+
+# описание массива в виде класса для использования в потоках по ссылке
 class MyArray:
     arr = [ ]
     changed = False
@@ -12,6 +16,7 @@ def Work(arg):
         arg[0].changed = True # установка флага, что массив изменен
     
 N = 100 # размер массива
+M = 4 # количество потоков
 
 array = MyArray() # создание объекта
 array.arr = [ random.randint(-N, N) for i in range(N) ] # генерация случайного массива
@@ -24,11 +29,12 @@ array.changed = True
 
 # пока массив изменяется
 while array.changed:
+    pool = multiprocessing.dummy.Pool(M) # создание пула потоков
     array.changed = False
-    
-    for i in range(len(array.arr) - 1):
-        Work([ array, i ])
-        
+    # каждому потоку передается ссылка на объект массива и четный/нечетный индекс элемента в зависимости от итерации
+    pool.map(Work, [ [ array, i * 2 + niter % 2 ] for i in range((N - niter % 2) // 2) ])
+    pool.close()
+    pool.join()
     niter += 1 # увеличение счетчика итераций
     
 print('Всего итераций: ' + str(niter))
